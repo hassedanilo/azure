443d966744454c9f256e41090319b90cadf9ca2a
hassedanilo <hassedanilo@gmail.com>
2023-01-18 16:38:12 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -2,9 +2,5 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-
 def hello():
-return "Hello, World!"
-
-# def homepage():
-# return render_template("homepage.html")
+    return "Hello, World!"
