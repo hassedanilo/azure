433fbbf170e2df70331f6b36302f4c531336face
hassedanilo <hassedanilo@gmail.com>
2023-01-18 16:02:25 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,5 +2,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello, World!"
+
+# def hello():
+#    return "Hello, World!"
+
+def homepage():
+return render_template("homepage.html")
